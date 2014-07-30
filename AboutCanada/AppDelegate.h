55c6fb7ae6b817dcee6fddd4f7a3fa6361c058cd
Suo Qin <kpsesuo@gmail.com>
2014-07-30 21:22:54 +0800

    Initial Commit

---
+++ AboutCanada/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AboutCanada
+//
+//  Created by kpse on 7/30/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
